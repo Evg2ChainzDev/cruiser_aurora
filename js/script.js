a05339c95483299051cf854be29bac9ba9404829
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2021-07-30 22:40:55 +0300

    acordeon done 100% ( fixed underlines )

--- js/script.js
+++ js/script.js
@@ -25,16 +25,26 @@ for (let i = 0; i < footerColumn.length-1; i++) {
             let arrowVarD2R = document.querySelector('.accordeon-footer-active .footer-column__title__arrow')
             arrowVarD2R.src='img/svg/arrow-right.svg';
             footerColumn[i].classList.remove("accordeon-footer-active")
-
+            let x = footerColumn[i].querySelector('.footer-column__title');
+            console.log('click'+x)
+            x.classList.add("footer-column__title_underlined")
             // console.log(arrowVarD2R) ;
 
         } else {
             // console.log('condition2_noActiveAccordeonClassHere') ;
-            for (let j = 0; j < footerColumn.length-1; j++) {
+            for (let j = 0; j < footerColumn.length - 1; j++) {
                 footerColumn[j].classList.remove("accordeon-footer-active");
-                // console.log('removing all active classes ')
 
+                // console.log('removing all active classes ')
             }
+            //to all titles add underlines start
+            let y = document.querySelectorAll('.footer-column__title');
+            console.log(y);
+            for (let g=0; g<y.length-1;g++){
+                y[g].classList.add('footer-column__title_underlined');
+            }
+            //to all titles add underlines end
+
             // console.log(document.getElementsByClassName('footer-column__title__arrow'))
             let arrOfAllArrows = document.getElementsByClassName('footer-column__title__arrow')
             // console.log('arrOfAllArrows = ' + arrOfAllArrows);
@@ -47,7 +57,9 @@ for (let i = 0; i < footerColumn.length-1; i++) {
             let arrowVarR2D = document.querySelector('.accordeon-footer-active .footer-column__title__arrow')
             // console.log(arrowVarR2D) ;
             arrowVarR2D.src='img/svg/arrow-bottom.svg';
-
+            let z = footerColumn[i].querySelector('.footer-column__title_underlined');
+            console.log('click'+z)
+            z.classList.remove("footer-column__title_underlined")
             // console.log($(footerColumn[i]).children('h3'))
             // footerColumn[i].classList.toggle('footer-column__title_underlined');
 
