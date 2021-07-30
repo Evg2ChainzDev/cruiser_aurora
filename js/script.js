d6839ad89951245a71ba36c758ebfccae16ac4f0
Evg2ChainzDev <88166547+Evg2ChainzDev@users.noreply.github.com>
2021-07-30 04:17:34 +0300

    styles devided into compomemts

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function () {
-    console.log('jqury work')
+
     $('.header__burgerIco').click(function(event) {
         $('.header__burgerIco').toggleClass('header__burgerIco_act');
         $('.burger-fullmenu').toggleClass('burger-fullmenu_disp-none');
-        console.log('burger clicked')
+        // console.log('burger clicked')
         }
     )
 
@@ -29,15 +29,15 @@ for (let i = 0; i < footerColumn.length-1; i++) {
             // console.log(arrowVarD2R) ;
 
         } else {
-            console.log('condition2_noActiveAccordeonClassHere') ;
+            // console.log('condition2_noActiveAccordeonClassHere') ;
             for (let j = 0; j < footerColumn.length-1; j++) {
                 footerColumn[j].classList.remove("accordeon-footer-active");
                 // console.log('removing all active classes ')
 
             }
-            console.log(document.getElementsByClassName('footer-column__title__arrow'))
+            // console.log(document.getElementsByClassName('footer-column__title__arrow'))
             let arrOfAllArrows = document.getElementsByClassName('footer-column__title__arrow')
-            console.log('arrOfAllArrows = ' + arrOfAllArrows);
+            // console.log('arrOfAllArrows = ' + arrOfAllArrows);
             //all arrows to right
             for (let z = 0; z < arrOfAllArrows.length-1; z++) {
                 arrOfAllArrows[z].src='img/svg/arrow-right.svg'
@@ -47,6 +47,10 @@ for (let i = 0; i < footerColumn.length-1; i++) {
             let arrowVarR2D = document.querySelector('.accordeon-footer-active .footer-column__title__arrow')
             // console.log(arrowVarR2D) ;
             arrowVarR2D.src='img/svg/arrow-bottom.svg';
+
+            // console.log($(footerColumn[i]).children('h3'))
+            // footerColumn[i].classList.toggle('footer-column__title_underlined');
+
 
         }
     }
